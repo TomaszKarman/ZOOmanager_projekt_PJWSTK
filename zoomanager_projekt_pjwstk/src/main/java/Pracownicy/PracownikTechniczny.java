03d7ace0f705f9a39dc25e3a51f291ce13e27812
TomaszKarman <s14642@pjwstk.edu.pl>
2016-11-21 13:05:40 +0100

    dodanie klas zwierze i pracownikTechniczny

---
+++ zoomanager_projekt_pjwstk/src/main/java/Pracownicy/PracownikTechniczny.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pracownicy;
+
+/**
+ *
+ * @author TKK
+ */
+public class PracownikTechniczny {
+    private String kod;
+    private String imie;
+    private String nazwisko;
+    private double pensja;
+    
+    
+    
+    //private void naprawWybieg ( Wybieg wybieg) {
+    //}
+    
+}
